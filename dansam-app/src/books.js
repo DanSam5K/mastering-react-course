f8d548786f6fb1b7b9261503aa749206a948f38c
Daniel Samuel <dansam5000@gmail.com>
2021-02-12 12:24:45 +0100

    best selling books react project

---
+++ dansam-app/src/books.js
@@ -0,0 +1,49 @@
+export const books=[
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51KxOW7R-AL._AC_UL200_SR200,200_.jpg",
+    title: 'Harvey The Heart Had Too Many Farts',
+    author: 'Jane Bexley',
+    briefbio: "A Rhyming Read Aloud Story Book For Kids And Adults About Farting and Friendship, A Valentine's Day Gift For Boys and Girls Paperback"
+  },
+  
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/610O4MLOT2L._AC_UL200_SR200,200_.jpg",
+    title: 'Greenlights',
+    author: 'Matthew McConaughey',
+    briefbio: "Unflinchingly honest and remarkably candid, Matthew McConaughey’s book invites us to grapple with the lessons of his life as he did—and to see that the point was never to win, but to understand.”—Mark Manson, author of The Subtle Art of Not Giving a F*ck"    
+  },
+
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81BfEN90D2L._AC_UL200_SR200,200_.jpg",
+    title: "Forgiving What You Can't Forget",
+    author: 'Lysa TerKeurst',
+    briefbio: "Discover How to Move On, Make Peace with Painful Memories, and Create a Life That’s Beautiful Again "
+  },
+
+  {
+    id: 4,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81PxN0B7CmL._AC_UL200_SR200,200_.jpg",
+    title: "Magnolia Table",
+    author: 'Joanna Gaines',
+    briefbio: "Magnolia Table is infused with Joanna Gaines' warmth and passion for all things family, prepared and served straight from the heart of her home, with recipes inspired by dozens of Gaines family favorites and classic comfort selections from the couple's new Waco restaurant, Magnolia Table" 
+  },
+  {
+    id: 5,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81-349iYbfL._AC_UL200_SR200,200_.jpg",
+    title: "Where the Crawdads Sing",
+    author: "Delia Owens",
+    briefbio: "For years, rumors of the 'Marsh Girl' have haunted Barkley Cove, a quiet town on the North Carolina coast. So in late 1969, when handsome Chase Andrews is found dead, the locals immediately suspect Kya Clark, the so-called Marsh Girl."
+  },
+
+  {
+    id: 6,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91yj3mbz4JL._AC_UL200_SR200,200_.jpg",
+    title: "The Intelligent Investor:",
+    author: "Benjamin Graham",
+    briefbio: "The greatest investment advisor of the twentieth century, Benjamin Graham, taught and inspired people worldwide. Graham's philosophy of 'value investing'-- which shields investors from substantial error and teaches them to develop long-term strategies"
+  }
+  
+] 
--- dansam-app/src/index.js
+++ dansam-app/src/index.js
@@ -1,2 +1,35 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+//import modules
+import { books } from './books';
+import Book from './Book'
+
+//css
+import './index.css';
+
+//setup vars
+
+
+function BookList() {
+  return (
+    <section className="booklist">
+      {books.map((book)=> {
+        return <Book key={book.id} book={book}></Book>;
+      })}
+    </section>
+  );
+}
+
+
+//attribute, eventHandler onClick
+//  const clickHandler = () => {
+//    alert('Hello Daniel')
+//  }
+
+ 
+
+ReactDom.render(<BookList />, document.getElementById('root'));
+
 
 
