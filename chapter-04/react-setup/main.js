42387aead2934cbe0694f849653b0f99948602ee
Daniel Samuel <dansam5000@gmail.com>
2024-04-18 16:30:08 +0200

    creat react element

--- chapter-04/react-setup/main.js
+++ chapter-04/react-setup/main.js
@@ -3,5 +3,18 @@ const element = React.createElement('h1', { id: 'recipe-0' }, 'Hello React');
 const dish = React.createElement('h2', null, 'baked bean');
 const dessert = React.createElement('h2', null, 'Coconut Cream Pie');
 
-console.log(element);
-ReactDOM.render([element, dish, dessert], document.getElementById('root'));
+const list = React.createElement(
+  'ul',
+  null,
+  React.createElement('li', null, '2 lb salmon'),
+  React.createElement('li', null, '5 sprigs fresh rosemary'),
+  React.createElement('li', null, '2 tablespoons olive'),
+  React.createElement('li', null, '2 small lemons'),
+  React.createElement('li', null, '1 Teaspoon kosher salt'),
+  React.createElement('li', null, '4 cloves of chopped garlic')
+);
+console.log(list);
+ReactDOM.render(
+  [element, dish, dessert, list, completePage],
+  document.getElementById('root')
+);
