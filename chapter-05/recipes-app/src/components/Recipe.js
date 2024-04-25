1396265175dc46eebff9b8b48b9b945812d1d33f
Daniel Samuel <dansam5000@gmail.com>
2024-04-25 13:04:38 +0200

    Recipe component module

---
+++ chapter-05/recipes-app/src/components/Recipe.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Recipe({ name, ingredients, steps }) {
+  return (
+    <section id={name.toLowerCase().replace(/ /g, '-')}>
+      <h1>{name}</h1>
+      <ul className="ingredients">
+        {ingredients.map((ingredient, i) => (
+          <li key={i}>
+            {ingredient.name}
+            {ingredient.amount}
+            {ingredient.measurement}
+          </li>
+        ))}
+      </ul>
+      <section className="instructions">
+        <h2>Cooking Instructions</h2>
+        {steps.map((step, i) => (
+          <p key={i}>{step}</p>
+        ))}
+      </section>
+    </section>
+  );
+}
